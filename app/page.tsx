21605b02e7b33177bdc5ae9a8fbe8aa5ce65dfa5
Ashutosh Mishra <iashutosh9119@gmail.com>
2024-09-17 18:59:08 +0900

    added background video

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { ArrowUpRightIcon } from "lucide-react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import type { Metadata } from "next";
 
@@ -15,88 +6,26 @@ export const metadata: Metadata = {
   description: "Frontend Engineer",
 };
 
-const projects = [
-  {
-    title: "Timebrew",
-    description: "A personal time tracker",
-    link: "https://github.com/siddharthroy12/timebrew",
-  },
-  {
-    title: "Agrus",
-    description: "A simple and stupid reddit clone",
-    link: "https://github.com/siddharthroy12/Agrus",
-  },
-  {
-    title: "Recoded",
-    description: "Make recordings of your code",
-    link: "https://github.com/siddharthroy12/recoded",
-  },
-  {
-    title: "Gravity sandbox",
-    description: "2D Newtonian gravity simulator",
-    link: "https://github.com/siddharthroy12/recoded",
-  },
-  {
-    title: "Touch Typer",
-    description: "Touch Typing Test on OpenGL",
-    link: "https://github.com/siddharthroy12/TouchTyper",
-  },
-  {
-    title: "Rockets",
-    description: "Dodge rockets in retro style",
-    link: "https://www.lexaloffle.com/bbs/?pid=111184",
-  },
-  {
-    title: "Big Text Meme generator",
-    description: "Generate FUNNY BIG TEXTS",
-    link: "https://big-text-meme-generator.vercel.app/",
-  },
-  {
-    title: "Raylib CMake Template",
-    description: "Start making games using C/C++ and Raylib in a minute",
-    link: "https://github.com/siddharthroy12/raylib-cmake-template",
-  },
-  {
-    title: "Hypersonic",
-    description: "3D space shooter demo",
-    link: "https://github.com/siddharthroy12/Hypersonic",
-  },
-];
-
 export default function Home() {
   return (
-    <ScrollArea className="grow w-full h-full">
-      <main className="max-w-[50rem] mx-auto px-8 py-20">
+    <ScrollArea className="grow w-full h-full relative">
+      {/* Background video */}
+      <video
+  autoPlay
+  muted
+  loop
+  className="absolute top-0 left-0 w-full h-full object-cover -z-11"
+>
+  <source src="shower2.mp4" type="video/mp4" />
+  Your browser does not support the video tag.
+</video>
+
+
+      <main className="relative z-10 max-w-[50rem] mx-auto px-8 py-20 text-white">
         <p className="mb-8">
-          A software engineer who builds all kinds of software, including web apps, mobile apps, CLI tools, automation scripts, and even dumb games.
+          A software engineer who builds all kinds of software, including web
+          apps, mobile apps, CLI tools, automation scripts, and even dumb games.
         </p>
-        <h2 className="mt-8 mb-4 text-xl font-semibold">Projects</h2>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[200px]">Title</TableHead>
-              <TableHead>Description</TableHead>
-              <TableHead className="text-right">Link</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {projects.map((project) => (
-              <TableRow key={project.title}>
-                <TableCell className="font-medium">{project.title}</TableCell>
-                <TableCell>{project.description}</TableCell>
-                <TableCell className="text-right">
-                  <a
-                    href={project.link}
-                    target="_blank"
-                    className="flex underline justify-end"
-                  >
-                    Open <ArrowUpRightIcon className="w-[20px]" />
-                  </a>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
       </main>
     </ScrollArea>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,149 +1,103 @@
-import { PlusIcon } from "lucide-react";
-import { Metadata } from "next";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { ArrowUpRightIcon } from "lucide-react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Journey",
-  description: "From being born to becoming developer",
+  title: "Ashutosh Mishra",
+  description: "Frontend Engineer",
 };
 
-const timeline = [
+const projects = [
   {
-    year: "2024",
-    events: [
-      {
-        title: "Joined Algo One",
-        description: "Developing full stack applications now with Python, React and Flutter.",
-      },
-    ],
+    title: "Timebrew",
+    description: "A personal time tracker",
+    link: "https://github.com/siddharthroy12/timebrew",
   },
   {
-    year: "2023",
-    events: [
-      {
-        title: "Started working on a time tracker app.",
-        description:
-          "I needed a cross-platform app to track my work hours, so I started making one using Flutter.",
-      },
-      {
-        title: "Contributed to a Open-Source game.",
-        description: "Check out SM63Redux. It's an awesome Mario fan-game.",
-      },
-      {
-        title: "Forked linux touchpad driver.",
-        description:
-          "My laptop has some hardware issues with the touchpad, so I had to modify the driver to make it usable. Thanks Dell 💩.",
-      },
-      {
-        title: "Made games using PICO-8.",
-        description:
-          "Discovered an awesome virtual console for making pixel-art games using Lua.",
-      },
-    ],
+    title: "Agrus",
+    description: "A simple and stupid reddit clone",
+    link: "https://github.com/siddharthroy12/Agrus",
   },
   {
-    year: "2022",
-    events: [
-      {
-        title: "Got my first job at Everlytics 🎉.",
-        description:
-          "Started as an intern, and after some time, became the frontend lead.",
-      },
-      {
-        title: "Made touch typing test in C++.",
-        description:
-          "I wanted a fast offline version of MonkeyType, so I implemented it myself.",
-      },
-      {
-        title: "Made full-stack MVP reddit clone.",
-        description:
-          "After learning MERN stack from Udemy. My first big project.",
-      },
-    ],
+    title: "Recoded",
+    description: "Make recordings of your code",
+    link: "https://github.com/siddharthroy12/recoded",
   },
   {
-    year: "2021",
-    events: [
-      {
-        title: "Made a game in C.",
-        description: "A bullet-hell shooter game made using Raylib.",
-      },
-    ],
+    title: "Gravity sandbox",
+    description: "2D Newtonian gravity simulator",
+    link: "https://github.com/siddharthroy12/recoded",
   },
   {
-    year: "2020",
-    events: [
-      {
-        title: "Started learning web development 👨‍💻.",
-        description: "From FreeCodeCamp",
-      },
-    ],
+    title: "Touch Typer",
+    description: "Touch Typing Test on OpenGL",
+    link: "https://github.com/siddharthroy12/TouchTyper",
   },
   {
-    year: "2017",
-    events: [
-      {
-        title: "Discovered programming and linux 👀.",
-        description: "My programming journey begins here.",
-      },
-    ],
+    title: "Rockets",
+    description: "Dodge rockets in retro style",
+    link: "https://www.lexaloffle.com/bbs/?pid=111184",
   },
   {
-    year: "2003",
-    events: [
-      {
-        title: "Born 👶.",
-        description: "On 30th November.",
-      },
-    ],
+    title: "Big Text Meme generator",
+    description: "Generate FUNNY BIG TEXTS",
+    link: "https://big-text-meme-generator.vercel.app/",
+  },
+  {
+    title: "Raylib CMake Template",
+    description: "Start making games using C/C++ and Raylib in a minute",
+    link: "https://github.com/siddharthroy12/raylib-cmake-template",
+  },
+  {
+    title: "Hypersonic",
+    description: "3D space shooter demo",
+    link: "https://github.com/siddharthroy12/Hypersonic",
   },
 ];
 
-export default function Journey() {
+export default function Home() {
   return (
-    <div className="w-full py-20">
-      <div className="px-8 max-w-4xl mx-auto w-full">
-        <div className="w-full">
-          <h1 className="text-3xl font-semibold mb-6">Journey</h1>
-          <div className="flex flex-col items-stretch gap-12">
-            {timeline.map((year) => (
-              <div
-                className="flex flex-col items-baseline gap-6 md:flex-row md:gap-12"
-                key={year.year}
-              >
-                <div className="flex items-center">
-                  <div className="font-semibold text-lg">{year.year}</div>
-                  <hr className="my-0 ml-4 flex-1 border-dashed border-gray-200" />
-                </div>
-                <section>
-                  {year.events.map((event, index) => (
-                    <div
-                      className="relative flex pb-8 last:pb-0"
-                      key={event.title}
-                    >
-                      {index !== year.events.length - 1 ? (
-                        <div className="absolute inset-0 flex w-6 items-center justify-center">
-                          <div className="pointer-events-none h-full w-px border-l-[1px]"></div>
-                        </div>
-                      ) : null}
-                      <div className="z-0 flex h-6 w-6 shrink-0 items-center justify-center rounded-full bg-primary align-middle text-primary-foreground">
-                        <PlusIcon width={16} height={16} />
-                      </div>
-                      <div className="flex-grow pl-8">
-                        <div className="word-break-word flex flex-col">
-                          <div className="font-semibold tracking-tight">
-                            {event.title}
-                          </div>
-                          <div className="text-sm">{event.description}</div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </section>
-              </div>
+    <ScrollArea className="grow w-full h-full">
+      <main className="max-w-[50rem] mx-auto px-8 py-20">
+        <p className="mb-8">
+          A software engineer who builds all kinds of software, including web apps, mobile apps, CLI tools, automation scripts, and even dumb games.
+        </p>
+        <h2 className="mt-8 mb-4 text-xl font-semibold">Projects</h2>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[200px]">Title</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead className="text-right">Link</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {projects.map((project) => (
+              <TableRow key={project.title}>
+                <TableCell className="font-medium">{project.title}</TableCell>
+                <TableCell>{project.description}</TableCell>
+                <TableCell className="text-right">
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    className="flex underline justify-end"
+                  >
+                    Open <ArrowUpRightIcon className="w-[20px]" />
+                  </a>
+                </TableCell>
+              </TableRow>
             ))}
-          </div>
-        </div>
-      </div>
-    </div>
+          </TableBody>
+        </Table>
+      </main>
+    </ScrollArea>
   );
 }
